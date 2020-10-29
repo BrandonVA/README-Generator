681e34ce08a09372204b73c566405efbd850da33
BrandonVA <codebva@gmail.com>
2020-10-29 14:21:56 -0700

    Added object to contain badges for different licenses.

--- createReadme.js
+++ createReadme.js
@@ -1,3 +1,12 @@
+const licenseBadges = {
+    ISC: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+    MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    GNU: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    Apache_2: '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    BSD_3: '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+
+}
+
 const generateReadMe = (inquirerObj) => {
     const { title, description, installInstructions, usageInfo, email, github, contribution, license, test } = inquirerObj;
 
